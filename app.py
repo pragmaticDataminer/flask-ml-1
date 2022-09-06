877a1cdf369eedc7ebdf556f729e71e03275800b
KA <kusumsanand@gmail.com>
2022-09-06 22:12:04 +0530

    removed depracated lib

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-#from sklearn.externals import joblib
 import joblib
 from sklearn.preprocessing import StandardScaler
 
